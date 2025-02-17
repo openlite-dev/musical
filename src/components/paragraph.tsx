45d8c92b3eb15472c9e7500fa6c471a9093c9d1b
sebastiandotdev <sebastiandotdev@gmail.com>
2025-02-17 11:04:33 -0500

    Fix declared but never used

--- src/components/paragraph.tsx
+++ src/components/paragraph.tsx
@@ -2,5 +2,5 @@ import { Text, type TextProps } from 'react-native'
 import { atoms as a } from '@musical/alf'
 
 export default function UParagraph({ style, ...rest }: TextProps) {
-  return <Text style={[a.text_md]} {...rest} />
+  return <Text style={[a.text_md, style]} {...rest} />
 }
