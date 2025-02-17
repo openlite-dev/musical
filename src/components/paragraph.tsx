4432f4c392bd9627bec1a1bfec70686e2b8e5fa1
sebastiandotdev <sebastiandotdev@gmail.com>
2025-02-17 11:02:45 -0500

    Enable testing powered by Jest

---
+++ src/components/paragraph.tsx
@@ -0,0 +1,6 @@
+import { Text, type TextProps } from 'react-native'
+import { atoms as a } from '@musical/alf'
+
+export default function UParagraph({ style, ...rest }: TextProps) {
+  return <Text style={[a.text_md]} {...rest} />
+}
